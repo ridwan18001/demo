b3a76d083b1c4926f7ce1f4819b5cfc91ef61d5e
ridwan18001 <ridwan18001@mail.unpad.ac.id>
2022-07-14 11:49:17 +0700

    Commit

---
+++ src/main/java/latihan1/demo/Day14juliApplication.java
@@ -0,0 +1,13 @@
+package latihan1.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day14juliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day14juliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/latihan1/demo/Day14juliApplicationTests.java
@@ -0,0 +1,13 @@
+package latihan1.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day14juliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
